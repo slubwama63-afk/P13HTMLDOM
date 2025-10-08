15cd172e96df3006f65202e18af861c0500c2982
slubwama63-afk <slubwama63@gmail.com>
2025-10-08 14:07:42 +0200

    Added decrease button

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 const text= document.getElementById('text');
 text.innerText = 'Sandra';
 text.style.color = 'pink';
-
+//Increase button
 let score = 0;
 
 function increaseBy(inputNumber) {
   score += inputNumber;
 }
-
+//Update score button
 function updateScore() {
   const text = document.getElementById('text');
   text.innerText = score;
@@ -18,10 +18,22 @@ button.addEventListener('click', function() {
   increaseBy(1);
   updateScore();
 });
-
+//Reset button
 function resetScore() {
   score = 0;
   updateScore();
 }
 const resetButton = document.getElementById('buttonReset');
 resetButton.addEventListener('click', resetScore);
+
+//Decrease button
+function decreaseBy(inputNumber) {
+  score -= inputNumber;
+}
+const decreaseButton = document.getElementById('buttonDecrease');
+decreaseButton.addEventListener('click', function() {
+  decreaseBy(1);
+  updateScore();
+});
+
+
